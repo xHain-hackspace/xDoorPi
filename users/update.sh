fe7b8748ec6a116ab858c93bc388b8629eefddda
Björn Reimer <git@bjrm.de>
2017-03-25 15:59:27 +0100

    remove old update script

--- users/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp ./authorized_keys /home/open/.ssh/authorized_keys
-cp ./authorized_keys /home/close/.ssh/authorized_keys
-
-chown open:open /home/open/.ssh/authorized_keys
-chown close:close /home/close/.ssh/authorized_keys
-
-echo "OK"
